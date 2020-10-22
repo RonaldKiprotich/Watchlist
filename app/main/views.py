ed3b0759cc662e691d979d3542158c9e02025014
RonaldKiprotich <protich12@gmail.com>
2020-10-22 19:23:46 +0300

    display

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,12 @@
 from flask import render_template,request,redirect,url_for
-from app import app
-from .request import get_movies,get_movie,search_movie
-from .models import review
+from . import main
+from ..request import get_movies,get_movie,search_movie
 from .forms import ReviewForm
-
-
-Review = review.Review
+from ..models import Review
 
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
@@ -29,7 +26,7 @@ def index():
         return redirect(url_for('search',movie_name=search_movie))
     else:
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
-@app.route('/movie/<int:id>')
+@main.route('/movie/<int:id>')
 def movie(id):
 
     '''
@@ -41,7 +38,7 @@ def movie(id):
 
     return render_template('movie.html',title = title,movie = movie,reviews = reviews)
     
-@app.route('/search/<movie_name>')
+@main.route('/search/<movie_name>')
 def search(movie_name):
     '''
     View function to display the search results
@@ -52,7 +49,7 @@ def search(movie_name):
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
 
-@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY=0b04d5d57c2bd061fd6d7d7fea57f91d
+export SECRET_KEY=ray1234
+
+python3.8 manage.py server
